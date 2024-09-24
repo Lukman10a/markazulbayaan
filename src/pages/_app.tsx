ad3849b7148e4ac7a05de540eda5291c5b6bc917
Lukman10a <abdulrauflukman9@gmail.com>
2024-09-24 12:27:18 +0100

    add cms

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import BlogLayout from "../components/BlogLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
