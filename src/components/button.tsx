8586f77b9a91b909956315bca2db8dd7de2da8d8
Lukman10a <abdulrauflukman9@gmail.com>
2024-12-27 09:12:17 +0100

    cleanups

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -17,7 +17,7 @@ const Button: React.FC<ButtonProps> = ({
   onClick,
 }) => {
   const buttonClasses = cn(
-    "font-semibold bg-yellow-500 hover:bg-[#0D1363] hover:text-white py-2 px-6 my-4 rounded-md",
+    "font-semibold bg-[#0D1363] hover:bg-yellow-500 hover:text-black text-white py-2 px-6 my-4 rounded-2xl",
     className
   );
 
--- src/components/home/offer.tsx
+++ src/components/home/offer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SubjectCard } from "../subjectcard";
 import Button from "../button";
 import Link from "next/link";
+// import discover_bg from "../../../public/assets/discover_bg.jpeg";
 
 export default function Offer() {
   return (
@@ -22,19 +23,19 @@ export default function Offer() {
       </div>
 
       <div className=" max-h-[600px] h-[100vmin] w-[100%] flex justify-center items-center ">
-        <div className="relative px-[10vw] md:px-[5vw] py-[8vmin] bg-[url('../../public/assets/journey_bg.png')] sm:bg-[url('../../public/assets/journey_bg2.png')] bg-top object-cover bg-cover w-[95%] md:w-[85%] lg:w-[80%] h-full rounded-[20px] md:rounded-[50px]">
-          <h1 className="text-[8vmin] text-white md:text-[8vmin] lg:text-[9vmin] font-bold mb-[3.5vmin] leading-tight tracking-tight w-[90%]  ">
+        <div className="relative px-[10vw] md:px-[5vw] py-[8vmin] bg-[url('../../public/assets/discover_bg.jpeg')] object-cover bg-cover w-[95%] md:w-[85%] lg:w-[80%] rounded-3xl md:rounded-[50px]">
+          <h1 className="text-[8vmin]  md:text-[8vmin] lg:text-[9vmin] font-bold mb-[3.5vmin] leading-tight tracking-tight w-[90%]  ">
             Embark on
             <br /> Your Journey
             <br /> Today
           </h1>
-          <p className="text-[3vmin]  text-white sm:text-[2vmin] leading-relaxed font-medium w-[100%] md:w-[50%] xl:w-[35%] bg-[#352605a8] p-4 ">
+          <p className="font-semibold text-xl">
             Step into a sanctuary of learning where tradition meets modernity.
             Our esteemed institution offers a comprehensive curriculum designed
             to nurture a deep understanding of Islamic principles and values.
           </p>
           <Link href={"https://bit.ly/Learn-at-markazbayan"}>
-            <div className="my-[2.5vmin] md:absolute bottom-[20px] right-[50px]">
+            <div className="">
               <Button text="Start now" />
             </div>
           </Link>
