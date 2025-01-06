6e1585b56393fa2f1c346be03e1f83a18927105b
Lukman10a <abdulrauflukman9@gmail.com>
2025-01-06 15:42:48 +0100

    ...

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { Hero } from "../hero";
-import Testimonials from "../testimonial";
+// import Testimonials from "../testimonial";
 
-import Faq from "../Faq";
-import CommunitySection from "./CommunitySection";
-import StatSection from "./StatSection";
-import Discover from "./discover";
-import Offer from "./offer";
-import Learn from "./learn";
+// import Faq from "../Faq";
+// import CommunitySection from "./CommunitySection";
+// import StatSection from "./StatSection";
+// import Discover from "./discover";
+// import Offer from "./offer";
+// import Learn from "./learn";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <StatSection />
+      {/* <StatSection />
       <Discover />
       <Offer />
       <Testimonials />
       <Learn />
       <CommunitySection />
-      <Faq />
+      <Faq /> */}
     </>
   );
 }
