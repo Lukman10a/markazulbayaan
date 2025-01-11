c3af0c6c079ca1c139130ea715d8f4378327a26c
luqmaan <obalolaluqman16@gmail.com>
2025-01-11 12:33:27 +0100

    second hero and course offer section

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -10,11 +10,50 @@ import Embark from "./embark";
 import TestimonialCarousel from "./newTestimonial";
 import Learn from "./learn";
 import WhyChoose from "./whyChoose";
+import HeroSection from "../SecondHeroSection";
+import OfferSection from "../OfferSection";
 
 export default function HomePage() {
+
+  const offers = [
+    {
+      title: "Arabic",
+      description: "Gain fluency in the Arabic language, the key to understanding Islamic texts.",
+      link: "/course/arabic",
+      imageSrc: "/assets/hadith.png",
+      imageAlt: "arabic books"
+    },
+    {
+      title: "Quran",
+      description: "Master Quranic recitation, memorization, and Tafseer.",
+      link: "/course/quran",
+      imageSrc: "/assets/quran.png",
+      imageAlt: "the noble quran"
+    },
+    {
+      title: "Hadith",
+      description: "Learn the sayings of the Prophet Muhammad.",
+      link: "/course/hadith",
+      imageSrc: "/assets/hadith.png",
+      imageAlt: "hadith books"
+    },
+    {
+      title: "Fiqh",
+      description: "Understand Islamic jurisprudence and law.",
+      link: "/course/fiqh",
+      imageSrc: "/assets/quran.png",
+      imageAlt: "fiqh books"
+    },
+  ];
+
   return (
     <>
       <Hero />
+      <HeroSection 
+        title="Discover the Essence of learning the Deen"
+        description="Whether you're a beginner seeking to grasp the fundamentals or an advanced learner striving for mastery, our expert educators are dedicated to guiding you every step of the way."
+      />
+      <OfferSection offers={offers} />
       <Embark />
       <TestimonialCarousel />
       {/* 
